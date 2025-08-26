382efd1b1366a454e23615df64dbb06bd254f4d4
utnim2 <mintugogoi567@gmail.com>
2025-08-26 23:49:17 +0530

    nit: linter issue

--- packages/nextjs/src/test/mocks/next-link.tsx
+++ packages/nextjs/src/test/mocks/next-link.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // Mock next/link
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const NextLink = ({ href, children, ...props }: any) => {
   return (
     <a href={href} {...props}>
