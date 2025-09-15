e427a5975410f95d83d80128890f815ccb0f10f4
JamesVictor-o <victorjames40v@gmail.com>
2025-09-15 22:40:37 +0100

    feat: Implement landing page hero section

--- packages/nextjs/src/app/(landing)/page.tsx
+++ packages/nextjs/src/app/(landing)/page.tsx
@@ -1,9 +1,10 @@
 import CommunitySection from '@/components/landing/CommunitySection';
 import Benefits from '@/components/landing/Benefits';
-
+import HeroSection from '@/components/landing/HeroSection';
 export default function LandingPage() {
   return (
     <div>
+      <HeroSection/>
       <Benefits />
       <CommunitySection />
     </div>
