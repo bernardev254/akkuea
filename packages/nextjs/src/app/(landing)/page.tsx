6e004e7b0ea04b9772f4431bc5c64af972a81ea8
Clement Raymond <chinexzy37@gmail.com>
2025-09-21 19:23:52 +0000

    removed wrong routing

--- packages/nextjs/src/app/(landing)/page.tsx
+++ packages/nextjs/src/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import CommunitySection from '@/components/landing/CommunitySection';
 import Benefits from '@/components/landing/Benefits';
 import Roadmap from '@/components/landing/Roadmap';
+import ResourceRating from "@/components/rating/Rating";
 
 export default function LandingPage() {
   return (
     <div>
       <Benefits />
+      <ResourceRating />
       <Roadmap />
       <CommunitySection />
     </div>
