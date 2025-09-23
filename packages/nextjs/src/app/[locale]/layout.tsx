e472117009fd2c17f8ab6a18827c8e70ff339547
marvelousmicheal <donatusmarvelous09@gmail.com>
2025-09-23 19:45:57 +0100

    feat:Add i18n support for multiple languages

--- packages/nextjs/src/app/[locale]/layout.tsx
+++ packages/nextjs/src/app/[locale]/layout.tsx
@@ -13,10 +13,10 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: (typeof routing.locales)[number]}>;
+  params: { locale: (typeof routing.locales)[number] };
 }) {
   // Ensure that the incoming `locale` is valid
-  const { locale } = await params;
+  const { locale } = params;
   if (!routing.locales.includes(locale)) {
     notFound();
   }
